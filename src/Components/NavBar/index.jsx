1a1474837158fdeb73fd85b6e821f5245119d0a8
Kaique Lira <Kaiqueliira@gmail.com>
2023-02-12 00:41:35 -0300

    :recycle: refactor: adicionado link para navegação na logo do header

--- src/Components/NavBar/index.jsx
+++ src/Components/NavBar/index.jsx
@@ -1,23 +1,36 @@
+import { Button } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
 import * as React from "react";
 import { GiOrange } from "react-icons/gi";
-
+import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <AppBar position="relative">
-      <Toolbar
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          gap: 1,
-        }}
-      >
-        <GiOrange />
-        <Typography variant="h6" color="inherit" noWrap>
-          Orange
-        </Typography>
+      <Toolbar>
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            gap: 1,
+          }}
+        >
+          <GiOrange />
+          <Typography
+            href="/"
+            variant="h6"
+            color="inherit"
+            noWrap
+            component="a"
+            sx={{
+              textDecoration: "none",
+            }}
+          >
+            Orange
+          </Typography>
+        </Box>
       </Toolbar>
     </AppBar>
   );
