90a95e02e1eb6c5d07a62fc5f53a667f5b3aac60
Kaique Lira <Kaiqueliira@gmail.com>
2023-02-12 00:58:49 -0300

    :recycle: refactor: adição de paramentros no componente

--- src/Components/CardCustom/index.jsx
+++ src/Components/CardCustom/index.jsx
@@ -6,7 +6,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import * as React from "react";
 
-export default function CardCustom(params) {
+export default function CardCustom({ id, title, description, courses }) {
   return (
     <Card
       sx={{
@@ -17,25 +17,18 @@ export default function CardCustom(params) {
     >
       <CardMedia
         component="img"
-        sx={{
-          // 16:9
-          pt: "56.25%",
-        }}
-        image="https://source.unsplash.com/random"
+        image="https://d335luupugsy2.cloudfront.net/cms/files/107693/1639661491/$s20z34nynz"
         alt="random"
+        sx={{ p: 2, background: "#c2c2c2" }}
       />
       <CardContent sx={{ flexGrow: 1 }}>
         <Typography gutterBottom variant="h5" component="h2">
-          Heading
+          {title}
         </Typography>
-        <Typography>
-          This is a media card. You can use this section to describe the
-          content.
-        </Typography>
+        <Typography>{description}</Typography>
       </CardContent>
       <CardActions>
-        <Button size="small">View</Button>
-        <Button size="small">Edit</Button>
+        <Button size="small">Ver Cursos</Button>
       </CardActions>
     </Card>
   );
