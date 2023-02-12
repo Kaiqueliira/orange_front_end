780da195f6ada6339ef34f2647a44d3f1d5e2a9e
Kaique Lira <Kaiqueliira@gmail.com>
2023-02-12 00:59:42 -0300

    :recycle: deixando pagina de Trilhas com o componente card dinamico

--- src/Pages/Trail/index.jsx
+++ src/Pages/Trail/index.jsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -8,19 +9,32 @@ import Grid from "@mui/material/Grid";
 import * as React from "react";
 import { GiOrange } from "react-icons/gi";
 import CardCustom from "../../Components/CardCustom";
+import Footer from "../../Components/Footer";
+import NavBar from "../../Components/NavBar";
 
-const cards = [0, 1, 2, 3, 4, 5];
+const cards = [
+  { id: 1, title: "Front-end", description: "Essa é a trilha de Frontend" },
+  { id: 2, title: "Back-end", description: "Essa é a trilha de Backend" },
+  { id: 3, title: "DevOps", description: "Essa é a trilha de DevOps" },
+];
 export default function Trail(params) {
   return (
-    <Container sx={{ py: 8 }} maxWidth="md">
-      {/* End hero unit */}
-      <Grid container spacing={4}>
-        {cards.map((card) => (
-          <Grid item key={card} xs={12} sm={6} md={4}>
-            <CardCustom />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <>
+      <NavBar />
+      <Container sx={{ py: 8 }} maxWidth="md">
+        <Typography variant="h2" mb={2}>
+          Trilhas
+        </Typography>
+        {/* End hero unit */}
+        <Grid container spacing={4}>
+          {cards.map(({ id, title, description }) => (
+            <Grid item key={id} xs={12} sm={6} md={4}>
+              <CardCustom title={title} description={description} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <Footer />
+    </>
   );
 }
